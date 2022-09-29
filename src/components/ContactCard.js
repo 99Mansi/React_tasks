df2babcf9e836b21ef49bbdec8de8c1cfd904cfe
mansinamdev <mansi.namdev@yash.com>
2022-09-29 12:33:49 +0530

    Added add functionality and rendered contactlist using use-state

---
+++ src/components/ContactCard.js
@@ -0,0 +1,21 @@
+import React from  'react';
+import user from '../images/user.png';
+
+const ContactCard = (props) => {
+    const { name , email } = props.contact;
+  return (
+    <div className="item">
+      <img src={user} className="ui avatar image" alt="user" />
+      <div className="content">
+        <div className="header">{name}</div>
+        <div>{email}</div>
+      </div>
+      <i
+        className="trash alternate outline icon"
+        style={{ color: "red", marginTop: "7px", float: "right" }}
+      ></i>
+    </div>
+  );
+};
+
+export default ContactCard;
